950446ea5a40d1cd35b981a55acf2b250ede1f71
Jaedsonn <128820369+Jaedsonn@users.noreply.github.com>
2023-10-14 19:45:15 -0300

    Add files via upload

---
+++ Interface de ligação/script.js
@@ -0,0 +1,85 @@
+function clicou1() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[0]
+  a.value = a.value + b.value
+}
+
+function clicou2() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[1]
+   
+  a.value = a.value + b.value
+}
+
+function clicou3() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[2]
+ 
+  a.value = a.value + b.value
+}
+
+function clicou4() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[3]
+  
+  a.value = a.value + b.value
+}
+
+function clicou5() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[4]
+   
+  a.value = a.value + b.value
+}
+
+function clicou6() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[5]
+ 
+  a.value = a.value + b.value
+}
+function clicou7() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[6]
+  
+  a.value = a.value + b.value
+}
+
+function clicou8() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[7]
+   
+  a.value = a.value + b.value
+}
+
+function clicou9() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[8]
+ 
+  a.value = a.value + b.value
+}
+
+function clicou10() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[9]
+  
+  a.value = a.value + b.value
+}
+
+function clicou11() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[10]
+   
+  a.value = a.value + b.value
+}
+
+function clicou12() {
+  var a = document.getElementById('digite')
+  var b = document.getElementsByClassName('btn')[11]
+  a.value = a.value + b.value
+}
+
+function apagarLetra(){
+  var texto = document.getElementById("digite");
+  texto.value = texto.value.substring(0, texto.value.length - 1);
+}
